4afa79e46929a167e086fffb894b8a3aa55ae7d9
itsparasbisht <iamparasbisht@gmail.com>
2024-03-25 20:42:11 +0530

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,17 +1,45 @@
 import { PaperProvider } from "react-native-paper";
-import { AppRegistry, Text } from "react-native";
+import {
+  AppRegistry,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { expo } from "./app.json";
 import Home from "./screens/Home";
-import { StatusBar } from "expo-status-bar";
+import { NativeWindStyleSheet } from "nativewind";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  NativeWindStyleSheet.setOutput({
+    default: "native",
+  });
+
+  const [fontsLoaded] = useFonts({
+    "Poppins-Thin": require("./assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+  });
+
   return (
-    <PaperProvider>
-      <StatusBar backgroundColor="black" />
-      <Home />
-      <Text className="text-red-600">Home</Text>
-    </PaperProvider>
+    <SafeAreaView style={styles.safeArea} className="flex-1">
+      <PaperProvider>
+        <Home />
+      </PaperProvider>
+    </SafeAreaView>
   );
 }
 
 AppRegistry.registerComponent(expo.name, () => App);
+
+const styles = StyleSheet.create({
+  safeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
-import { ActivityIndicator, MD2Colors } from "react-native-paper";
+import { ActivityIndicator } from "react-native-paper";
 import { useLocation } from "../hooks/useLocation";
+
+const locationIcon = require("../assets/location.jpg");
 
 export default function Home() {
   const { location, errorMsg } = useLocation();
@@ -9,13 +11,20 @@ export default function Home() {
   if (location === undefined && errorMsg === "") {
     return (
       <View className="flex-1 items-center justify-center bg-white">
-        <ActivityIndicator animating={true} color={MD2Colors.red800} />
+        <ActivityIndicator
+          animating={true}
+          className="text-blue-700"
+          size={"large"}
+        />
       </View>
     );
   } else if (errorMsg) {
     return (
-      <View>
-        <Text>{errorMsg}</Text>
+      <View className="flex-1 items-center justify-center bg-white -mt-10">
+        <Image source={locationIcon} className="w-40 h-40" />
+        <Text className="text-base font-[Poppins-Medium] text-gray-700">
+          {errorMsg}
+        </Text>
       </View>
     );
   }
